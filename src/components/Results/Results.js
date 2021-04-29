1f3efa3fa091d1cc62154e56177f6a2722d4f527
raaynaldo <raaynaldo@hotmail.com>
2021-04-29 14:27:44 -0400

    update results component

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,7 +1,21 @@
 const Results = () => {
     return (
-        <div className='w-1/2 px-10 py-5 bg-white rounded-sm shadow-md'>
-            <h4>Results for ''</h4>
+        <div className='w-1/2 px-10 py-5 space-y-3 bg-white rounded-sm shadow-md'>
+            <h4>Results for "Pirate"</h4>
+            <ul className='ml-10 space-y-3 list-disc'>
+                <li>
+                    Rambo (1992) <button className='btn'>Nominate</button>
+                </li>
+                <li>
+                    Spiderman (2002) <button className='btn'>Nominate</button>
+                </li>
+                <li>
+                    Avatar <button className='btn'>Nominate</button>
+                </li>
+                <li>
+                    Avatar <button className='btn'>Nominate</button>
+                </li>
+            </ul>
         </div>
     );
 };
