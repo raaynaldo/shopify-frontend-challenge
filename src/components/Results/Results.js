a15f6b7dd5a308b2650c676fed91b19c047fc5da
raaynaldo <raaynaldo@hotmail.com>
2021-04-29 14:16:56 -0400

    results component

---
+++ src/components/Results/Results.js
@@ -0,0 +1,9 @@
+const Results = () => {
+    return (
+        <div className='w-1/2 px-10 py-5 bg-white rounded-sm shadow-md'>
+            <h4>Results for ''</h4>
+        </div>
+    );
+};
+
+export default Results;
