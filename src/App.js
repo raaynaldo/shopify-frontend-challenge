23f202f766f9ebaba18556e182e23ed40a3f9b2f
raaynaldo <raaynaldo@hotmail.com>
2021-04-29 14:27:52 -0400

    app js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Search from 'components/Search/Search';
 
 const App = () => {
     return (
-        <div className='flex flex-col items-center h-screen p-10 space-y-6 bg-gray-100'>
-            <Search />
-            <div className='flex w-3/4 space-x-6'>
-                <Results />
-                <Nominations />
+        <>
+            <div className='flex flex-col items-center h-screen p-10 space-y-6 bg-gray-100'>
+                <div className='w-3/4'>
+                    <h3>The Shopies</h3>
+                </div>
+                <Search />
+                <div className='flex w-3/4 space-x-6'>
+                    <Results />
+                    <Nominations />
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
