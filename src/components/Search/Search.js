6dc6c8886e909b3618a0bbaca03d2b9a7acad28c
raaynaldo <raaynaldo@hotmail.com>
2021-04-29 14:15:44 -0400

    search component

---
+++ src/components/Search/Search.js
@@ -0,0 +1,13 @@
+const Search = () => {
+    return (
+        <div className='w-3/4 px-10 py-5 bg-white rounded-sm shadow-md'>
+            <div>Movie Title</div>
+            <input
+                type='text'
+                className='w-full border-2 border-gray-200 rounded-sm'
+            />
+        </div>
+    );
+};
+
+export default Search;
