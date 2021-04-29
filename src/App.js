dda44a3fea91856789995fcf49c323ec5517c0ee
raaynaldo <raaynaldo@hotmail.com>
2021-04-29 14:48:43 -0400

    pass state to search component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import Nominations from 'components/Nominations/Nominations';
 import Results from 'components/Results/Results';
 import Search from 'components/Search/Search';
 
 const App = () => {
+    const [searchInput, setSearchInput] = useState('');
+
     return (
         <>
             <div className='flex flex-col items-center h-screen p-10 space-y-6 bg-gray-100'>
                 <div className='w-3/4'>
                     <h3>The Shopies</h3>
                 </div>
-                <Search />
+                <Search
+                    searchInput={searchInput}
+                    setSearchInput={setSearchInput}
+                />
                 <div className='flex w-3/4 space-x-6'>
                     <Results />
                     <Nominations />
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,14 @@
-const Search = () => {
+const Search = ({ searchInput, setSearchInput }) => {
     return (
         <div className='w-3/4 px-10 py-5 bg-white rounded-sm shadow-md'>
             <div>Movie Title</div>
             <input
                 type='text'
                 className='w-full border-2 border-gray-200 rounded-sm'
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
             />
+            <button className='btn'>Search</button>
         </div>
     );
 };
