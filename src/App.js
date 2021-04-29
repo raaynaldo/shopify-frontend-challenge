d91f597e2052688cb73f30c00676ea99b729713d
raaynaldo <raaynaldo@hotmail.com>
2021-04-29 14:15:31 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
+import Nominations from 'components/Nominations/Nominations';
+import Results from 'components/Results/Results';
+import Search from 'components/Search/Search';
+
+const App = () => {
     return (
-        <div className='flex items-center justify-center h-screen'>
-            <div className='text-center'>Tailwind Included</div>
+        <div className='flex flex-col items-center h-screen p-10 space-y-6 bg-gray-100'>
+            <Search />
+            <div className='flex w-3/4 space-x-6'>
+                <Results />
+                <Nominations />
+            </div>
         </div>
     );
-}
+};
 
 export default App;
