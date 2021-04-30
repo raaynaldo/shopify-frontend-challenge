64586a92b34c4e8b8b484cc93bed4e0a1d5ca232
raaynaldo <raaynaldo@hotmail.com>
2021-04-29 21:47:10 -0400

    implement add to nomination functionality

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,20 +1,19 @@
-const Results = () => {
+const Results = ({ results, AddToNominations }) => {
     return (
         <div className='w-1/2 px-10 py-5 space-y-3 bg-white rounded-sm shadow-md'>
-            <h4>Results for "Pirate"</h4>
+            <h4>Results for "{results.searchInput}"</h4>
             <ul className='ml-10 space-y-3 list-disc'>
-                <li>
-                    Rambo (1992) <button className='btn'>Nominate</button>
-                </li>
-                <li>
-                    Spiderman (2002) <button className='btn'>Nominate</button>
-                </li>
-                <li>
-                    Avatar <button className='btn'>Nominate</button>
-                </li>
-                <li>
-                    Avatar <button className='btn'>Nominate</button>
-                </li>
+                {results.data.map((result) => (
+                    <li key={`result-${result.imdbID}`}>
+                        {result.Title} ({result.Year}){' '}
+                        <button
+                            className='btn'
+                            onClick={() => AddToNominations(result)}
+                        >
+                            Nominate
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     );
