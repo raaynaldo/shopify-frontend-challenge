c780324beb45214dc1629d35fe7b26f26cffc6e5
raaynaldo <raaynaldo@hotmail.com>
2021-04-29 21:46:35 -0400

    create form and update search box css

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,17 @@
-const Search = ({ searchInput, setSearchInput }) => {
+const Search = ({ searchInput, setSearchInput, fetchData }) => {
     return (
-        <div className='w-3/4 px-10 py-5 bg-white rounded-sm shadow-md'>
+        <form
+            className='w-3/4 px-10 py-5 space-y-2 bg-white rounded-sm shadow-md'
+            onSubmit={(e) => fetchData(e)}
+        >
             <div>Movie Title</div>
             <input
                 type='text'
-                className='w-full border-2 border-gray-200 rounded-sm'
+                className='w-full px-3 py-2 text-sm border-2 border-gray-200 rounded-sm'
                 value={searchInput}
                 onChange={(e) => setSearchInput(e.target.value)}
             />
-            <button className='btn'>Search</button>
-        </div>
+        </form>
     );
 };
 
