ef59cdeaff42881a6d4555a4d5f34af9d5139f75
raaynaldo <raaynaldo@hotmail.com>
2021-04-29 21:48:02 -0400

    implement remove nomination

--- src/components/Nominations/Nominations.js
+++ src/components/Nominations/Nominations.js
@@ -1,20 +1,14 @@
-const Nominations = () => {
+const Nominations = ({ nominations }) => {
     return (
         <div className='w-1/2 px-10 py-5 space-y-3 bg-white rounded-sm shadow-md'>
             <h4>Nominations</h4>
             <ul className='ml-10 space-y-3 list-disc'>
-                <li>
-                    Rambo (1992) <button className='btn'>Remove</button>
-                </li>
-                <li>
-                    Spiderman (2002) <button className='btn'>Remove</button>
-                </li>
-                <li>
-                    Avatar <button className='btn'>Remove</button>
-                </li>
-                <li>
-                    Avatar <button className='btn'>Remove</button>
-                </li>
+                {nominations.map((nomination) => (
+                    <li key={`nomination-${nomination.imdbID}`}>
+                        {nomination.Title} ({nomination.Year}){' '}
+                        <button className='btn'>Remove</button>
+                    </li>
+                ))}
             </ul>
         </div>
     );
